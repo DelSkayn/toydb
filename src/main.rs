9b32ca2c2b80cb2546c2cb57bb183711b7fb00cf
Mees Delzenne <mees.delzenne@gmail.com>
2023-11-21 15:55:32 +0100

    A new tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut tree = dbg!(art::Art::<str, usize>::new());
+    let mut tree = (art::Art::<str, usize>::new());
     tree.display();
     tree.insert("hello world", 1);
     dbg!(&tree);
@@ -55,7 +55,12 @@ fn main() {
     assert_eq!(tree.get("hello foo").copied(), Some(4));
     assert_eq!(tree.get("hello boo").copied(), Some(5));
     assert_eq!(tree.get("hello voo").copied(), Some(6));
+    assert_eq!(tree.get("hello voa").copied(), Some(7));
 
+    tree.insert(
+        "hello a very very long prefix that doesn't fit into an inlined buffer.",
+        8,
+    );
     dbg!(&tree);
     tree.display();
     assert_eq!(tree.get("hello world").copied(), Some(1));
@@ -65,4 +70,25 @@ fn main() {
     assert_eq!(tree.get("hello boo").copied(), Some(5));
     assert_eq!(tree.get("hello voo").copied(), Some(6));
     assert_eq!(tree.get("hello voa").copied(), Some(7));
+    assert_eq!(
+        tree.get("hello a very very long prefix that doesn't fit into an inlined buffer.")
+            .copied(),
+        Some(8)
+    );
+
+    tree.insert("hello world\0 null byte", 9);
+    tree.display();
+    assert_eq!(tree.get("hello world").copied(), Some(1));
+    assert_eq!(tree.get("hello moon ").copied(), Some(2));
+    assert_eq!(tree.get("h").copied(), Some(3));
+    assert_eq!(tree.get("hello foo").copied(), Some(4));
+    assert_eq!(tree.get("hello boo").copied(), Some(5));
+    assert_eq!(tree.get("hello voo").copied(), Some(6));
+    assert_eq!(tree.get("hello voa").copied(), Some(7));
+    assert_eq!(
+        tree.get("hello a very very long prefix that doesn't fit into an inlined buffer.")
+            .copied(),
+        Some(8)
+    );
+    assert_eq!(tree.get("hello world\0 null byte").copied(), Some(9));
 }
